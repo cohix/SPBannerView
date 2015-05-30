1f16f0182e22e8910d0a73e28187b32ec857e409
Connor Hicks <connor@spottedu.co>
2015-05-30 15:55:56 -0400

    Initial Commit

---
+++ SPBannerView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SPBannerView
+//
+//  Created by Connor MacBook on 2015-05-30.
+//  Copyright (c) 2015 cohix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
